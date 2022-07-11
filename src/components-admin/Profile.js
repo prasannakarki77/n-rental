f9bf6be6bb3bdcf6b89f65deff085c6677e5a96e
prasannakarki77 <prasanna.karki77@gmail.com>
2022-07-11 18:47:28 +0545

    added profile update fuctionality

--- src/components-admin/Profile.js
+++ src/components-admin/Profile.js
@@ -5,9 +5,13 @@ import "../styles/profile.scss";
 import { FaUserAlt, FaKey } from "react-icons/fa";
 import { MdEmail, MdAddPhotoAlternate } from "react-icons/md";
 import { BsTelephoneFill } from "react-icons/bs";
+import UpdateProfile from "../components-user/UpdateProfile";
+import UpdateProfileImage from "../components-user/UpdateProfileImage";
 
 const Profile = () => {
-  const [details, setDetails] = useState("");
+  const [updateForm, setUpdateFormShow] = useState(false);
+  const [imageUpdate, setImageUpdate] = useState(false);
+  const [profile, setProfile] = useState([]);
   const config = {
     headers: {
       Authorization: "Bearer " + localStorage.getItem("userToken"),
@@ -17,7 +21,7 @@ const Profile = () => {
     axios
       .get("http://localhost:90/user/dashboard", config)
       .then((res) => {
-        setDetails(res.data);
+        setProfile(res.data);
         console.log(res);
       })
       .catch((e) => {
@@ -30,31 +34,38 @@ const Profile = () => {
     window.location.replace("/login");
   };
 
+  const updateHandler = () => {
+    setUpdateFormShow(true);
+  };
+  const imageUpdateHandler = () => {
+    setImageUpdate(true);
+  };
+
   return (
     <>
-      {/* <div className="dashboard-content">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-4">
-              <p>Dashboard</p>
-              <p>Username: {details.username} </p>
-              <p>Firstname: {details.firstname} </p>
-              <p>Lastname: {details.lastname} </p>
-              <p>Phone: {details.phone}</p>
-              <p>Email: {details.email}</p>
-              <button className="btn btn-primary me-2"> Update Profile</button>
-              <button onClick={logout} className="btn btn-danger">
-                {" "}
-                Logout
-              </button>
-            </div>
-          </div>
-        </div>
-      </div> */}
       <div className="profile">
+        <UpdateProfile
+          show={updateForm}
+          onHide={() => setUpdateFormShow(false)}
+          profile={profile}
+        />
+
+        <UpdateProfileImage
+          show={imageUpdate}
+          onHide={() => setImageUpdate(false)}
+          profile={profile}
+        />
         <div className="profile__image">
-          <MdAddPhotoAlternate />
-          <img src={car_drive} alt="profile_image" />
+          <MdAddPhotoAlternate onClick={imageUpdateHandler} />
+          {}
+          <img
+            src={
+              profile.profile_img
+                ? `http://localhost:90/${profile.profile_img}`
+                : car_drive
+            }
+            alt="profile_image"
+          />
         </div>
         <div className="profile__details container">
           <p>My Profile</p>
@@ -64,7 +75,7 @@ const Profile = () => {
             <p className="profile__detail--value">
               {" "}
               <FaUserAlt className="me-2" />
-              {details.username}
+              {profile.username}
             </p>
           </div>
           <div className="row">
@@ -73,14 +84,14 @@ const Profile = () => {
               <p className="profile__detail--value">
                 {" "}
                 <FaUserAlt className="me-2" />
-                {details.firstname ? details.firstname : "----------"}
+                {profile.firstname ? profile.firstname : "----------"}
               </p>
             </div>
             <div className="profile__detail col-md-6">
               <p className="profile__detail--label">Lastname</p>
               <p className="profile__detail--value">
                 <FaUserAlt className="me-2" />{" "}
-                {details.lastname ? details.lastname : "----------"}
+                {profile.lastname ? profile.lastname : "----------"}
               </p>
             </div>
           </div>
@@ -89,14 +100,14 @@ const Profile = () => {
               <p className="profile__detail--label">Address</p>
               <p className="profile__detail--value">
                 <FaUserAlt className="me-2" />{" "}
-                {details.address ? details.address : "----------"}
+                {profile.address ? profile.address : "----------"}
               </p>
             </div>
             <div className="profile__detail col-md-6">
               <p className="profile__detail--label">Gender</p>
               <p className="profile__detail--value">
                 <FaUserAlt className="me-2" />{" "}
-                {details.gender ? details.gender : "----------"}
+                {profile.gender ? profile.gender : "----------"}
               </p>
             </div>
           </div>
@@ -105,26 +116,24 @@ const Profile = () => {
               <p className="profile__detail--label">Email</p>
               <p className="profile__detail--value">
                 <MdEmail className="me-2" />{" "}
-                {details.email ? details.email : "----------"}
+                {profile.email ? profile.email : "----------"}
               </p>
             </div>
             <div className="profile__detail col-md-6">
               <p className="profile__detail--label">Phone</p>
               <p className="profile__detail--value">
                 <BsTelephoneFill className="me-2" />{" "}
-                {details.phone ? details.phone : "----------"}
+                {profile.phone ? profile.phone : "----------"}
               </p>
             </div>
           </div>
-          {/* <p>Username: {details.username} </p>
-          <p>Firstname: {details.firstname} </p>
-          <p>Lastname: {details.lastname} </p>
-          <p>Phone: {details.phone}</p>
-          <p>Email: {details.email}</p> */}
           <div className="row">
             <div className="col-md-6">
               {" "}
-              <button className="btn btn-primary me-2 profile__btn">
+              <button
+                className="btn btn-primary me-2 profile__btn"
+                onClick={updateHandler}
+              >
                 {" "}
                 Update Profile
               </button>
---
+++ src/components-user/UpdateProfile.js
@@ -0,0 +1,212 @@
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function UpdateProfile(props) {
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [address, setAddress] = useState("");
+  const [phone, setPhone] = useState("");
+  const [gender, setGender] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+
+  useEffect(() => {
+    if (firstname === "") {
+      setFirstname(props.profile.firstname);
+    }
+    if (lastname === "") {
+      setLastname(props.profile.lastname);
+    }
+    if (address === "") {
+      setAddress(props.profile.address);
+    }
+    if (phone === "") {
+      setPhone(props.profile.phone);
+    }
+    if (gender === "") {
+      setGender(props.profile.gender);
+    }
+    if (username === "") {
+      setUsername(props.profile.username);
+    }
+    if (email === "") {
+      setEmail(props.profile.email);
+    }
+  }, []);
+
+  const updateProfile = (e) => {
+    e.preventDefault();
+
+    const data = {
+      firstname: firstname,
+      lastname: lastname,
+      address: address,
+      phone: phone,
+      gender: gender,
+      username: username,
+      email: email,
+    };
+
+    const config = {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("userToken"),
+      },
+    };
+
+    axios
+      .put("http://localhost:90/user/update_profile", data, config)
+      .then((res) => {
+        if (res.data.success === true) {
+          window.location.replace("/profile");
+          console.log("Profile Updated Successfully");
+        } else {
+          console.log("Please Try Again! Something Went Wrong!!!");
+        }
+
+        console.log(res.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Update Profile
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Group className="mb-3" controlId="formGridAddress1">
+            <Form.Label>Vehicle Name</Form.Label>
+            <Form.Control
+              placeholder="Enter username"
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+              defaultValue={props.profile.username}
+            />
+          </Form.Group>
+          <Row className="mb-3">
+            <Form.Group as={Col} controlId="formGridPassword">
+              <Form.Label>Firstname</Form.Label>
+              <Form.Control
+                placeholder="Enter firstname"
+                onChange={(e) => {
+                  setFirstname(e.target.value);
+                }}
+                defaultValue={props.profile.firstname}
+              />
+            </Form.Group>
+            <Form.Group as={Col} controlId="formGridPassword">
+              <Form.Label>Lastname</Form.Label>
+              <Form.Control
+                placeholder="Enter lastname"
+                onChange={(e) => {
+                  setLastname(e.target.value);
+                }}
+                defaultValue={props.profile.lastname}
+              />
+            </Form.Group>
+          </Row>
+          <Row className="mb-3">
+            <Form.Group as={Col} controlId="formGridPassword">
+              <Form.Label>Phone</Form.Label>
+              <Form.Control
+                placeholder="Enter phone number"
+                onChange={(e) => {
+                  setPhone(e.target.value);
+                }}
+                defaultValue={props.profile.phone}
+              />
+            </Form.Group>
+            <Form.Group as={Col} controlId="formGridPassword">
+              <Form.Label>Address</Form.Label>
+              <Form.Control
+                placeholder="Enter address"
+                onChange={(e) => {
+                  setAddress(e.target.value);
+                }}
+                defaultValue={props.profile.address}
+              />
+            </Form.Group>
+          </Row>
+          <Row className="mb-3">
+            <Form.Group as={Col} controlId="formGridPassword">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                placeholder="Enter email address"
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+                defaultValue={props.profile.email}
+              />
+            </Form.Group>
+            <Form.Group as={Col} controlId="formGridPassword">
+              <Form.Label>Gender</Form.Label>
+              {/* <Form.Control
+                placeholder="Enter gender"
+                onChange={(e) => {
+                  setGender(e.target.value);
+                }}
+                defaultValue={props.profile.gender}
+              /> */}
+              <div key={`inline-radio`} className="mb-3">
+                <Form.Check
+                  inline
+                  label="Male"
+                  name="group1"
+                  type="radio"
+                  onChange={(e) => {
+                    setGender("Male");
+                  }}
+                  id={`inline-radio-1`}
+                />
+                <Form.Check
+                  inline
+                  label="Female"
+                  name="group1"
+                  type="radio"
+                  onChange={(e) => {
+                    setGender("Female");
+                  }}
+                  id={`inline-radio-2`}
+                />
+                <Form.Check
+                  inline
+                  label="Other"
+                  name="group1"
+                  type="radio"
+                  onChange={(e) => {
+                    setGender("Other");
+                  }}
+                  id={`inline-radio-3`}
+                />
+              </div>
+            </Form.Group>
+          </Row>
+
+          <Button variant="primary" type="submit" onClick={updateProfile}>
+            Submit
+          </Button>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+export default UpdateProfile;
