1a6f46e51a84c37cc41aef269cc3b28526672228
prasannakarki77 <prasanna.karki77@gmail.com>
2022-07-18 17:21:39 +0545

    displayed data count from api in dashboard

--- src/components-admin/AdminDashboard.js
+++ src/components-admin/AdminDashboard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { MdSpaceDashboard } from "react-icons/md";
 import { BiCategoryAlt } from "react-icons/bi";
 import { ImBlog } from "react-icons/im";
@@ -6,7 +6,56 @@ import { AiFillCar } from "react-icons/ai";
 import { BsFillBookmarksFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
 import "../styles/dashboard.scss";
+import axios from "axios";
 const AdminDashboard = () => {
+  const [vehicleCount, setVehicleCount] = useState();
+  const [categoryCount, setCategoryCount] = useState();
+  const [articleCount, setArticleCount] = useState();
+  const [bookingCount, setBookingCount] = useState();
+  const config = {
+    headers: {
+      Authorization: "Bearer " + localStorage.getItem("userToken"),
+    },
+  };
+  useEffect(() => {
+    axios
+      .get("http://localhost:90/vehicle/get", config)
+      .then((res) => {
+        setVehicleCount(Object.keys(res.data.data).length);
+        console.log(vehicleCount);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+
+    axios
+      .get("http://localhost:90/category/get", config)
+      .then((res) => {
+        setCategoryCount(Object.keys(res.data.data).length);
+        console.log(categoryCount);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+    axios
+      .get("http://localhost:90/article/get", config)
+      .then((res) => {
+        setArticleCount(Object.keys(res.data.data).length);
+        console.log(articleCount);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+    axios
+      .get("http://localhost:90/booking/get", config)
+      .then((res) => {
+        setBookingCount(Object.keys(res.data.data).length);
+        console.log(bookingCount);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, []);
   return (
     <div className="dashboard">
       <div className="dashboard__title">
@@ -20,7 +69,7 @@ const AdminDashboard = () => {
             <div className="dashboard__box--icon">
               <AiFillCar />
             </div>
-            <div className="dashboard__box--count">10</div>
+            <div className="dashboard__box--count">{vehicleCount}</div>
             <div className="dashboard__box--label">Vehicles</div>
           </div>
         </Link>
@@ -29,7 +78,7 @@ const AdminDashboard = () => {
             <div className="dashboard__box--icon">
               <BiCategoryAlt />
             </div>
-            <div className="dashboard__box--count">10</div>
+            <div className="dashboard__box--count">{categoryCount}</div>
             <div className="dashboard__box--label">Categories</div>
           </div>
         </Link>
@@ -38,16 +87,16 @@ const AdminDashboard = () => {
             <div className="dashboard__box--icon">
               <ImBlog />
             </div>
-            <div className="dashboard__box--count">10</div>
+            <div className="dashboard__box--count">{articleCount}</div>
             <div className="dashboard__box--label">Articles</div>
           </div>
         </Link>
-        <Link to="/bookmark_dashboard">
+        <Link to="/booking_dashboard">
           <div className="dashboard__box">
             <div className="dashboard__box--icon">
               <BsFillBookmarksFill />
             </div>
-            <div className="dashboard__box--count">10</div>
+            <div className="dashboard__box--count">{bookingCount}</div>
             <div className="dashboard__box--label">Bookings</div>
           </div>
         </Link>
